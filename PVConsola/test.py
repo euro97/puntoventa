308b80b0fea42b7e94af5d6b869740ff2f3e2c55
euro97 <62072371+euro97@users.noreply.github.com>
2026-04-05 00:30:47 -0600

    Delete PVConsola/test.py

--- PVConsola/test.py
+++
@@ -1,7 +0,0 @@
-import VentasDAO as vd
-import Ventas as v
-
-datos = vd.consultarVentas()
-
-for i in datos:
-    print(f"ID Venta: {i[0]}, ID Detalle: {i[1]}, Cliente: {i[4]}, Producto: {i[5]}, Cantidad: {i[7]}, Método de Pago: {i[3]}, Total: {i[2]}")
