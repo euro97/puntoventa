cee34bedc86f901019247b795e5786532e116ac9
Luis Vera <luisvera970429@gmail.com>
2026-04-05 18:05:31 -0600

    update

--- ScriptsSQL/base de datos.sql
+++ ScriptsSQL/base de datos.sql
@@ -25,3 +25,53 @@ INSERT INTO productos (nombre, precio, marca, cantidad, tamano) VALUES
 ('Shampoo', 65.00, 'Head & Shoulders', 8, 400);
 
 select * from productos;
+
+
+CREATE TABLE ventas (
+    id_venta SERIAL PRIMARY KEY,
+    fecha TIMESTAMP NOT NULL DEFAULT NOW(),
+    total NUMERIC(10,2) NOT NULL CHECK (total >= 0),
+    metodo_pago VARCHAR(50) NOT NULL,
+    cliente VARCHAR(100),
+    id_usuario INT,
+    
+    CONSTRAINT ventas_metodo_pago_check 
+        CHECK (metodo_pago IN ('Efectivo', 'Tarjeta', 'Transferencia'))
+);
+
+ALTER TABLE ventas
+ADD CONSTRAINT fk_usuario
+FOREIGN KEY (id_usuario)
+REFERENCES usuarios(id_usuario)
+ON DELETE SET NULL;
+
+
+
+CREATE TABLE detalle_venta (
+    id_detalle SERIAL PRIMARY KEY,
+    id_venta INT NOT NULL,
+    id_producto INT NOT NULL,
+    cantidad INT NOT NULL CHECK (cantidad > 0),
+    precio_unitario NUMERIC(10,2) NOT NULL CHECK (precio_unitario >= 0),
+
+    CONSTRAINT fk_venta
+        FOREIGN KEY (id_venta) REFERENCES ventas(id_venta)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_producto
+        FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    rol VARCHAR(50) NOT NULL DEFAULT 'Cajero',
+    activo BOOLEAN NOT NULL DEFAULT TRUE,
+    fecha_creacion TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT usuarios_rol_check
+        CHECK (rol IN ('Admin', 'Cajero'))
+);
